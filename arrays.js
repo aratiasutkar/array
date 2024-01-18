1342d82bf2844eda79529ebe8472c62d8f197b04
aratiasutkar <153296495+aratiasutkar@users.noreply.github.com>
2024-01-18 12:57:53 +0530

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,18 @@
+{
+    let fruit = ["fruit"];
+console.log(fruit[0]);
+}
+
+{
+    let fruit = ["apple", "banana", "orange"];
+
+    fruit.shift("apple")
+    fruit.push("grape")
+    fruit[1] = "pear";
+
+    console.log(fruit[0]);
+    console.log(fruit[1]);
+    console.log(fruit[2]);
+    console.log(fruit[3]);
+    
+}
